18efee274da2d01142a75f378298082b017cc04d
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2022-06-20 09:08:32 +0200

    nav fix

--- src/routes/book_bord.svelte
+++ src/routes/book_bord.svelte
@@ -21,47 +21,49 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<nav class="flex md:ml-8  ml-4 pt-4 -mb-4" aria-label="Breadcrumb">
-	<ol class="inline-flex items-center space-x-1 md:space-x-3">
-		<li class="inline-flex items-center">
-			<a
-				href="/"
-				class="inline-flex items-center text-sm font-medium text-gray-700 hover:underline dark:text-gray-400 dark:hover:underline"
-			>
-				<svg
-					class="mr-2 w-4 h-4"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
-					/></svg
-				>
-				Hjem
-			</a>
-		</li>
-		<li>
-			<div class="flex items-center">
-				<svg
-					class="w-6 h-6 text-gray-400"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						fill-rule="evenodd"
-						d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-						clip-rule="evenodd"
-					/></svg
-				>
+<section class="nav">
+	<nav class="flex md:ml-8  ml-4 pt-4 -mb-4" aria-label="Breadcrumb">
+		<ol class="inline-flex items-center space-x-1 md:space-x-3">
+			<li class="inline-flex items-center">
 				<a
-					href="book_bord"
-					class="ml-1 text-sm font-medium text-gray-700 hover:underline md:ml-2 dark:text-gray-400 dark:hover:underline"
-					>Book Bord</a
+					href="/"
+					class="inline-flex items-center text-sm font-medium text-gray-700 hover:underline dark:text-gray-400 dark:hover:underline"
 				>
-			</div>
-		</li>
-	</ol>
-</nav>
+					<svg
+						class="mr-2 w-4 h-4"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
+						/></svg
+					>
+					Hjem
+				</a>
+			</li>
+			<li>
+				<div class="flex items-center">
+					<svg
+						class="w-6 h-6 text-gray-400"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							fill-rule="evenodd"
+							d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+							clip-rule="evenodd"
+						/></svg
+					>
+					<a
+						href="book_bord"
+						class="ml-1 text-sm font-medium text-gray-700 hover:underline md:ml-2 dark:text-gray-400 dark:hover:underline"
+						>Book Bord</a
+					>
+				</div>
+			</li>
+		</ol>
+	</nav>
+</section>
 
 <section class="main">
 	<div class="book grid md:grid-cols-2">
@@ -116,6 +118,9 @@
 </section>
 
 <style>
+	.nav {
+		display: flex;
+	}
 	h1,
 	h2,
 	p {
